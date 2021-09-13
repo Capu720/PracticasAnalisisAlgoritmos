f983d6cb70c61ec30e2ee48dcadb9077233255f1
mauricioPacheco894 <mauriciojesus894@gmail.com>
2021-09-12 23:35:16 -0500

    Subidos burbuja con tiempo

---
+++ Practica 1/Con Tiempo/burbuja1T.c
@@ -0,0 +1,130 @@
+//*****************************************************************
+//M. EN C. EDGARDO ADRIÁN FRANCO MARTÍNEZ 
+//Curso: Análisis de algoritmos
+//(C) Enero 2013
+//ESCOM-IPN
+//Ejemplo de medición de tiempo en C y recepción de parametros en C bajo UNIX
+//Compilación: "gcc main.c tiempo.x  -o main(teimpo.c si se tiene la implementación de la libreria o tiempo.o si solo se tiene el codigo objeto)"
+//Ejecución: "./main n" (Linux y MAC OS)
+//*****************************************************************
+
+//*****************************************************************
+//LIBRERIAS INCLUIDAS
+//*****************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tiempo.h"
+//*****************************************************************
+//DEFINICION DE CONSTANTES DEL PROGRAMA
+//*****************************************************************
+
+//********************************************************************************
+//DECLARACION DE ESTRUCTURAS
+//********************************************************************************
+
+//*****************************************************************
+//DECLARACIÓN DE FUNCIONES
+//*****************************************************************
+void burbujaOpt1(int A[], int n);/*Protipo de la funcion*/
+/*
+*Recibe: Un areglo y el tamanio
+*Regresa: void
+*Errores: Ninguno
+*/
+
+//*****************************************************************
+//VARIABLES GLOBALES
+//*****************************************************************
+
+//*****************************************************************
+//PROGRAMA PRINCIPAL 
+//*****************************************************************
+int main (int argc, char* argv[])
+{	
+	//******************************************************************	
+	//Variables del main
+	//******************************************************************	
+	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
+	int n; 	//n determina el tamaño del algorito dado por argumento al ejecutar
+	int i; //Variables para loops
+    int *A; //Variable tipo apuntador para el arreglo
+
+	//******************************************************************	
+	//Recepción y decodificación de argumentos
+	//******************************************************************	
+
+	//Si no se introducen exactamente 2 argumentos (Cadena de ejecución y cadena=n)
+	if (argc!=2) 
+	{
+		printf("\nIndique el tamanio del algoritmo - Ejemplo: [user@equipo]$ %s 100\n",argv[0]);
+		exit(1);
+	} 
+	//Tomar el segundo argumento como tamaño del algoritmo
+	else
+	{
+		n=atoi(argv[1]);
+	}
+	A = (int*)malloc(sizeof(int)*n); /*Reserva memoria para el arreglo*/
+
+    /*Llena el arreglo*/
+    for (i = 0; i < n; i++){
+        scanf("%d",&A[i]);
+    }
+
+	//******************************************************************	
+	//Iniciar el conteo del tiempo para las evaluaciones de rendimiento
+	//******************************************************************	
+	uswtime(&utime0, &stime0, &wtime0);
+	//******************************************************************
+	
+	//******************************************************************	
+	//Algoritmo
+	//******************************************************************	
+	burbujaOpt1(A,n);/*Llamada a la funcion de ordenamiento*/
+	//******************************************************************
+
+	//******************************************************************	
+	//Evaluar los tiempos de ejecución 
+	//******************************************************************
+	uswtime(&utime1, &stime1, &wtime1);
+	
+	//Cálculo del tiempo de ejecución del programa
+	printf("\n");
+	printf("real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+	
+	//Mostrar los tiempos en formato exponecial
+	printf("\n");
+	printf("real (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+	//******************************************************************
+
+    free(A);/*Libera la memoria*/
+
+	//Terminar programa normalmente	
+	exit (0);	
+}
+
+//************************************************************************
+//DEFINICIÓN DE FUNCIONES 
+//************************************************************************
+void burbujaOpt1(int A[], int n){/*recibe un arreglo y el tamanio de este*/
+    int i, j, aux;/*declaracion de variables*/
+
+    for(i=0; i<=n-2; i++){
+        for(j=0; j<=n-2-i; j++){/*Optimizacion: Despues de cada iteracion el mayor queda ordenado, ya no se compara.*/
+            if(A[j]>A[j+1]){/*Si el elemento actual es mayor que el siguiente los intercambia*/
+                aux = A[j];
+                A[j] = A[j+1];
+                A[j+1] = aux;
+            }
+        }
+    }
+}
