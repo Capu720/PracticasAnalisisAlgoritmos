660f4aab385c2af671b55ee8eda86dd91ac3133e
mauricioPacheco894 <mauriciojesus894@gmail.com>
2021-09-15 00:36:16 -0500

    Valores de pruba para los script reducidos

--- Practica 1/Con Tiempo/insercionTie.sh
+++ Practica 1/Con Tiempo/insercionTie.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 gcc insercionTie.c tiempo.c -o ins
-./ins 100 < numeros10millones.txt > insercionTie.txt
-./ins 1000 < numeros10millones.txt >> insercionTie.txt
-./ins 5000 < numeros10millones.txt >> insercionTie.txt
+./ins 100   < numeros10millones.txt > insercionTie.txt
+./ins 1000  < numeros10millones.txt >> insercionTie.txt
+./ins 2000  < numeros10millones.txt >> insercionTie.txt
+./ins 3000  < numeros10millones.txt >> insercionTie.txt
+./ins 5000  < numeros10millones.txt >> insercionTie.txt
+./ins 8000  < numeros10millones.txt >> insercionTie.txt
 ./ins 10000 < numeros10millones.txt >> insercionTie.txt
+./ins 20000 < numeros10millones.txt >> insercionTie.txt
+./ins 30000 < numeros10millones.txt >> insercionTie.txt
+./ins 40000 < numeros10millones.txt >> insercionTie.txt
 ./ins 50000 < numeros10millones.txt >> insercionTie.txt
+./ins 60000 < numeros10millones.txt >> insercionTie.txt
+./ins 70000 < numeros10millones.txt >> insercionTie.txt
+./ins 80000 < numeros10millones.txt >> insercionTie.txt
+./ins 90000 < numeros10millones.txt >> insercionTie.txt
 ./ins 100000 < numeros10millones.txt >> insercionTie.txt
 ./ins 200000 < numeros10millones.txt >> insercionTie.txt
+./ins 300000 < numeros10millones.txt >> insercionTie.txt
 ./ins 400000 < numeros10millones.txt >> insercionTie.txt
-./ins 600000 < numeros10millones.txt >> insercionTie.txt
-./ins 800000 < numeros10millones.txt >> insercionTie.txt
-./ins 1000000 < numeros10millones.txt >> insercionTie.txt
-./ins 2000000 < numeros10millones.txt >> insercionTie.txt
-./ins 3000000 < numeros10millones.txt >> insercionTie.txt
-./ins 4000000 < numeros10millones.txt >> insercionTie.txt
-./ins 5000000 < numeros10millones.txt >> insercionTie.txt
-./ins 6000000 < numeros10millones.txt >> insercionTie.txt
-./ins 7000000 < numeros10millones.txt >> insercionTie.txt
-./ins 8000000 < numeros10millones.txt >> insercionTie.txt
-./ins 9000000 < numeros10millones.txt >> insercionTie.txt
-./ins 10000000 < numeros10millones.txt >> insercionTie.txt
+./ins 500000 < numeros10millones.txt >> insercionTie.txt
