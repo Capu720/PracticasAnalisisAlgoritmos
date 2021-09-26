5d4d914013b4181add12344afefa2adb9f23d609
mauricioPacheco894 <mauriciojesus894@gmail.com>
2021-09-26 14:09:38 -0500

    Cambios

--- Practica 2/Implementaciones/scriptv2.sh
+++ Practica 2/Implementaciones/scriptv2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-gcc lineal.c -o lineal
-gcc ABB.c -o ABB 
-gcc binaria.c -o binaria
-gcc exponencial.c -o exponencial
-gcc fibonacci.c -o fibonacci
+gcc lineal.c -o lineal.o
+gcc ABB.c -o ABB.o
+gcc binaria.c -o binaria.o
+gcc exponencial.c -o exponencial.o
+gcc fibonacci.c -o fibonacci.o
 
 n=(1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000)
 
@@ -11,9 +11,9 @@ for i in ${n[@]}
 do
 if [[ "$i" -eq "1000000" ]]
 then
-./lineal $i < numeros10millones.txt > salida.txt
+./lineal.o $i < numeros10millones.txt > salida.txt
 else
-./lineal $i < numeros10millones.txt >> salida.txt
+./lineal.o $i < numeros10millones.txt >> salida.txt
 fi
 done
 
@@ -21,20 +21,20 @@ echo "\n" >> salida.txt
 
 for i in ${n[@]}
 do
-./ABB $i < numeros10millones.txt >> salida.txt
+./ABB.o $i < numeros10millones.txt >> salida.txt
 done
 
 for i in ${n[@]}
 do
-./binaria $i < 10millones.txt >> salida.txt
+./binaria.o $i < 10millones.txt >> salida.txt
 done
 
 for i in ${n[@]}
 do
-./exponencial $i < 10millones.txt >> salida.txt
+./exponencial,o $i < 10millones.txt >> salida.txt
 done
 
 for i in ${n[@]}
 do
-./fibonacci $i < 10millones.txt >> salida.txt
+./fibonacci.o $i < 10millones.txt >> salida.txt
 done
