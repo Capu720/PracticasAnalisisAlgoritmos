7001141b9b21c8424d934f5ccd33d5340543cecc
AlbertoPC13 <BetucaPC13@hotmail.com>
2021-09-27 02:01:59 -0500

    Script actualizado

--- Practica 2/Implementaciones/script.sh
+++ Practica 2/Implementaciones/script.sh
@@ -3,7 +3,7 @@ gcc fibonacci.c -o fibonacci
 gcc binaria.c -o binaria
 gcc lineal.c -o lineal
 gcc exponencial.c -o exponencial
-#gcc arbol.c -o arbol   #porque beto aun no lo sube jsjsj
+gcc ABB.c -o arbol
 n=(1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000)
 
 for i in ${n[@]}
@@ -16,6 +16,6 @@ else
 ./binaria $i < 10millones.txt >> salida.txt
 ./lineal $i < 10millones.txt >> salida.txt
 ./exponencial $i < 10millones.txt >> salida.txt
-#./arbol $i < 10millones.txt >> salida.txt
+./arbol $i < 10millones.txt >> salida.txt
 fi
 done
