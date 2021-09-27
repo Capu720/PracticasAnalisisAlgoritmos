24c07a5723a6ab8c273235a3698077570c910bc4
mauricioPacheco894 <mauriciojesus894@gmail.com>
2021-09-27 11:09:44 -0500

    Intento implementacion lineal con hilos

--- Practica 2/Con Hilos/linealHilos.c
+++ Practica 2/Con Hilos/linealHilos.c
@@ -3,16 +3,15 @@
 #include <stdlib.h>
 #define NUM_HILOS 4
 
-int *A, n, x;
+int *A, n, x, pos;
 
-void * funHilo(void * arg);
-
-int busquedaLineal(int arr[], int n, int x);
+void * busquedaLineal(void * arg);
 
 int main(int argc, char *argv[]){
-    int register i;
+    int indices[NUM_HILOS];
+    int i;
     pthread_t hilos[NUM_HILOS];
-    int n = atoi(argv[1]);
+    n = atoi(argv[1]);
     A = malloc(sizeof(int) * n); /*Reserva memoria para el arreglo*/
     
     //Elementos que se buscaran
@@ -28,39 +27,35 @@ int main(int argc, char *argv[]){
     }
 
     x = 0;
+    pos=-1;
 
     for(i=0; i<NUM_HILOS; i++){
-        pthread_create(&hilos[i], NULL, funHilo, NULL);
+        indices[i]=i;
+        pthread_create(&hilos[i], NULL, busquedaLineal, (void*)&indices[i]);
     }
     for(i=0; i<NUM_HILOS; i++){
         pthread_join(hilos[i], NULL);
+        if (pos == -1)
+            printf("El elemento %d no esta en el arreglo\n", x);
+        else
+            printf("El elemento %d esta en la posicion %d\n", x, pos);
     }
 
     return 0;
 }
 
-void * funHilo(void * arg){
-    int i, tam, tamu, result;
-    tam = n/NUM_HILOS;
-    tamu = tam + n%NUM_HILOS;
+void *busquedaLineal(void * arg){
+    int i;
+    printf("%d %d %d ", n, x, pos);
+    if(pos != -1)
+        pthread_exit(NULL);
 
-    for (i = 0; i < n; i++){ //Recorre el arreglo
-        if (A[i] == x)    //Compara el elemento buscado con el de la posicion i
-            result = i;       //Regresa la posicion donde se encuentra
-        else result = -1;
+    for (i = 0; i < n; i++){//Recorre el arreglo
+        if (A[i] == x){     //Compara el elemento buscado con el de la posicion i
+            pos = i;        //Regresa la posicion donde se encuentra
+            pthread_exit(NULL);
+        }          
     }
-
-    if (result == -1)
-            printf("El elemento %d no esta en el arreglo\n", x);
-        else
-            printf("El elemento %d esta en la posicion %d\n", x, result);
+    pos = -1;
     pthread_exit(NULL);
-}
-
-int busquedaLineal(int arr[], int n, int x){
-    int i;                  //Declara el indice
-    for (i = 0; i < n; i++) //Recorre el arreglo
-        if (arr[i] == x)    //Compara el elemento buscado con el de la posicion i
-            return i;       //Regresa la posicion donde se encuentra
-    return -1;
 }
